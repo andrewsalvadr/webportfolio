43ee3295fd79fc528e6b7eca41f410918446f78a
andrewsalvadr <andrewsalva88@gmail.com>
2024-02-13 20:32:46 +0800

    rearrange

--- src/Projects/projectsData.js
+++ src/Projects/projectsData.js
@@ -7,20 +7,6 @@ const projectsData = [
         link: "https://amaika.ai/",
     },
     {
-      title: "Admin Dashboard",
-      image: "./admindashboard.png",
-      description:
-        "A basic Admin dashboard layout using html, vanilla css & javascript that can add, select or delete profile, upload photo & edit dashboard info.",
-        link: "https://admin-dashboard-tau-flax.vercel.app/",
-    },
-    {
-      title: "Talent Wise",
-      image: "./talentwise.png",
-      description:
-        "A responsive web-landing page of a recruitment agency developed using reactjs, tailwind css and some vanilla css.",
-        link: "https://mystaffonline.vercel.app/",
-    },
-    {
       title: "UrbanFit-Clothings",
       image: "./urbanfit-clothings.png",
       description:
@@ -35,11 +21,11 @@ const projectsData = [
         link: "https://cornpop-trailers.vercel.app/",
     },
     {
-      title: "Moo Chat",
-      image: "./moochat.png",
+      title: "Talent Wise",
+      image: "./talentwise.png",
       description:
-        "Chat messaging application developed using react-hooks, basic css, google firebase authentication and stored data to cloud firestore.",
-        link: "https://moo-chat.vercel.app/",
+        "A responsive web-landing page of a recruitment agency developed using reactjs, tailwind css and some vanilla css.",
+        link: "https://mystaffonline.vercel.app/",
     },
     {
       title: "Money Leaf",
@@ -61,6 +47,20 @@ const projectsData = [
       description:
       "A responsive static coffee supplier business website developed using html, tailwind, javascript with contact email form.",
       link: "https://spill-coffee.vercel.app/",
+    },
+    {
+      title: "Admin Dashboard",
+      image: "./admindashboard.png",
+      description:
+        "A basic Admin dashboard layout using html, vanilla css & javascript that can add, select or delete profile, upload photo & edit dashboard info.",
+        link: "https://admin-dashboard-tau-flax.vercel.app/",
+    },
+    {
+      title: "Moo Chat",
+      image: "./moochat.png",
+      description:
+        "Chat messaging application developed using react-hooks, basic css, google firebase authentication and stored data to cloud firestore.",
+        link: "https://moo-chat.vercel.app/",
     },
     {
       title: "Design by Decks",
